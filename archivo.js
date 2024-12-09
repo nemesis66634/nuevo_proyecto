2091c6f61b0199f0dd123ecbfa09868d44327d28
nemesis66634 <jacintotenezaca875@gmail.com>
2024-12-09 16:13:18 -0500

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -1,6 +1,18 @@
 
-let nombre = document.getElementById('div1');
+let nombre = parseint(document.getElementById('div1').value);
+let mostrar = document.getElementById('div1');
 
+function empezar{
+    if(nombre == "jhon"){
+      mostrar.innerHTML = `
+        <p>Hola Jhon</p>
+      `
+    } else {
+      mostrar.innerHTML = `
+        <p>Hola y ya</p>
+      `
+    }
 
+}
 
 
