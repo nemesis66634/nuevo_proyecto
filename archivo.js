2f6394e48e91c4010b36aa91c5705a2227f9ee23
nemesis66634 neme <jacintotenezaca875@gmail.com>
2025-01-26 22:19:02 +0000

    main

--- archivo.js
+++ archivo.js
@@ -1,21 +1,24 @@
-function mostrar() {
-    const input1 = document.getElementById("input1");
+
+function actualizarDiv() {
+    const valor = document.getElementById("input1").value;
     const div1 = document.getElementById("div1");
-
-    function actualizarDiv() {
-        const valor = input1.value;
-        if (valor === "nombre") {
-            div1.innerHTML = `<p>Tu nombre es: ${valor}</p>`;
-        } else {
-            div1.innerHTML = `<p>vaya nombre de m... ${valor} jaja</p>`;
-        }
+    if (valor === "nombre") {
+        div1.innerHTML = `<p>Tu nombre es: ${valor}</p>`;
+    } else {
+        div1.innerHTML = `<p>vaya nombre de m... ${valor} jaja</p>`;
     }
-
-    input1.addEventListener("input", actualizarDiv);
-
-    input1.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            actualizarDiv();
-        }
-    });
 }
+
+
+function mostrar() {
+    actualizarDiv();
+}
+
+
+const valor = document.getElementById("input1");
+valor.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        actualizarDiv();
+    }
+});
+
