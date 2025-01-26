a58efc907f0a58423d6b7a5b73dd9ff62fa9b2dd
nemesis66634 neme <jacintotenezaca875@gmail.com>
2025-01-26 21:54:48 +0000

    main

--- archivo.js
+++ archivo.js
@@ -14,6 +14,8 @@ function mostrar(){
 
     document.getElementById("input1").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
+            const input1 = document.getElementById("input1").value;
+            const div1 = document.getElementById("div1");
             if(input1 == "nombre"){
                 div1.innerHTML = `
                     <p>Tu nombre es : ${input1}</p>
@@ -25,4 +27,6 @@ function mostrar(){
             }
         }
     });
+
+
 }
