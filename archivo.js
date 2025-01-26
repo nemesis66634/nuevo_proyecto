37e7b150be7f03516b2927abcd78de9b94432735
nemesis66634 <jacintotenezaca875@gmail.com>
2025-01-26 16:09:54 -0500

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -2,11 +2,11 @@ function mostrar(){
     const input1 = document.getElementById("input1").value;
     const div1 = document.getElementById("div1");
     if(input1 == "nombre"){
-        input1.innerHTML = `
+        div1.innerHTML = `
             <p>Tu nombre es : ${input1}</p>
         `
     }else{
-        input1.innerHTML = `
+        div1.innerHTML = `
         <p>valla nombre de m...  ${input1}  jaja</p>
         `
     }
