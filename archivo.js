b57b7275576a7de69331a30716120feb5f92eae1
nemesis66634 neme <jacintotenezaca875@gmail.com>
2025-01-26 21:49:10 +0000

    main

--- archivo.js
+++ archivo.js
@@ -1,13 +1,28 @@
 function mostrar(){
     const input1 = document.getElementById("input1").value;
     const div1 = document.getElementById("div1");
+
     if(input1 == "nombre"){
         div1.innerHTML = `
             <p>Tu nombre es : ${input1}</p>
-        `
-    }else{
+        `;
+    } else {
         div1.innerHTML = `
-        <p>valla nombre de m...  ${input1}  jaja</p>
-        `
+            <p>valla nombre de m...  ${input1}  jaja</p>
+        `;
     }
+
+    document.getElementById("input1").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            if(input1 == "nombre"){
+                div1.innerHTML = `
+                    <p>Tu nombre es : ${input1}</p>
+                `;
+            } else {
+                div1.innerHTML = `
+                    <p>valla nombre de m...  ${input1}  jaja</p>
+                `;
+            }
+        }
+    });
 }
